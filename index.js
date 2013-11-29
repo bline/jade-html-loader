fdf7d1fdd4bc8fa735b84a741ceee9d103d36e6f
Scott Beck <scottbeck@gmail.com>
2013-11-29 18:15:01 +0400

    not using path, not client, only compile and return the string to filter through something else

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Scott Beck @bline
+*/
+var loaderUtils = require("loader-utils");
+module.exports = function(source) {
+	this.cacheable && this.cacheable(true);
+	var jade = require("jade");
+	var query = loaderUtils.parseQuery(this.query);
+	var req = loaderUtils.getRemainingRequest(this).replace(/^!/, "");
+	var tmplFunc = jade.compile(source, {
+		filename: req,
+		self: query.self,
+		pretty: query.pretty,
+		locals: query.locals,
+		compileDebug: this.debug || false
+	});
+
+	return tmplFunc(query);
+}
