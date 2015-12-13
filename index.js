b164913d682e5dcd573853a22684c5248f632385
Selkin Vitaly <contact@selkinvitaly.ru>
2015-12-13 16:28:53 +0300

    refactoring of index.js

--- index.js
+++ index.js
@@ -2,20 +2,24 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Scott Beck @bline
 */
+
 var loaderUtils = require("loader-utils");
+
 module.exports = function(source) {
-	this.cacheable && this.cacheable(true);
-	var jade = require("jade");
-	var query = loaderUtils.parseQuery(this.query);
-	var req = loaderUtils.getRemainingRequest(this).replace(/^!/, "");
+	this.cacheable && this.cacheable();
+
+	var jade     = require("jade");
+	var query    = loaderUtils.parseQuery(this.query);
+	var req      = loaderUtils.getRemainingRequest(this).replace(/^!/, "");
+
 	var tmplFunc = jade.compile(source, {
-		filename: req,
-		self: query.self,
-		pretty: query.pretty,
-		locals: query.locals,
-		doctype: query.doctype || 'html',
+		filename:     req,
+		self:         query.self,
+		pretty:       query.pretty,
+		locals:       query.locals,
+		doctype:      query.doctype || "html",
 		compileDebug: this.debug || false
 	});
 
 	return tmplFunc(query);
-}
+};
